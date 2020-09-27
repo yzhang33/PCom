7539f9b301ff37f915b0cd3e3dc9c5232831006c
Youming Zhang <youmingzhang19@gmail.com>
2020-09-27 17:43:40 -0400

    update base

--- base.h
+++ base.h
@@ -8,10 +8,10 @@
 #define NOTE_CS2 69
 #define NOTE_D2  73
 #define NOTE_DS2 78
-#define NOTE_E2  82
+#define NOTE_E2  82        //base guitar E string
 #define NOTE_F2  87
 #define NOTE_FS2 93
-#define NOTE_G2  98
+#define NOTE_G2  98        
 #define NOTE_GS2 104
-#define NOTE_A2  110
+#define NOTE_A2  110     //base guitar A string
 #define NOTE_AS2 117
